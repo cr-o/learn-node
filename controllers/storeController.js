44c8fa0a198ed030655ae1cb0cc34469609c98ab
cat beans <workemailzoop@gmail.com>
2020-01-08 13:32:42 -0500

    up to middleware

---
+++ controllers/storeController.js
@@ -0,0 +1,10 @@
+exports.myMiddleware = (req, res, next) => {
+    req.name = 'myname';
+    req.cookie('name', 'this is a cookie', {maxAge: 9000000});
+    next();
+};
+
+exports.homePage = (req, res) => {
+    console.log(req.name);
+    res.render('index');
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const storeController = require('../controllers/storeController');
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+
+router.get('/', storeController.myMiddleware, storeController.homePage);
+//router.get('/', (req, res) => {
+  // res.send('Hey! It works!');
+  // res.render('hello', {name: 'myDog', dog: req.query.dog, title: 'working title'});
+//});
 
 module.exports = router;
