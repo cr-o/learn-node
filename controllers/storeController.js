86344ab163d9cc3a137d91fb3945f602fc69be88
cat beans <workemailzoop@gmail.com>
2020-01-09 17:54:51 -0500

    async await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,4 +1,23 @@
+
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store'); // getting export at end of store.js
+
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index');
+};
+
+exports.addStore = (req, res) => {
+    res.render('editStore' , {title: 'Add Store'});
+};
+
+exports.createStore = async (req, res) => {
+    const store = new Store(req.body); // pass from body to store
+    await store.save();
+    console.log('It worked!');
+    res.redirect('/');
+    // store.age = 10; // these won't be reflected in DB until saved
+    // store.save();
+    // // console.log(req.body);
+    // // res.json(req.body);
 };
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 // Do work here
 
+//object destructuring lets us import an entire object. we are importing the one method we need.
+const {catchErrors} = require('../handlers/errorHandlers');
+
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 //router.get('/', (req, res) => {
   // res.send('Hey! It works!');
   // res.render('hello', {name: 'myDog', dog: req.query.dog, title: 'working title'});
