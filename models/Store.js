59e71f25bcd2b0079545c0654cdb923fb5fd7208
cat beans <workemailzoop@gmail.com>
2020-01-15 16:26:47 -0500

    Using Pre-Save hooks to make Unique Slugs

--- models/Store.js
+++ models/Store.js
@@ -37,12 +37,23 @@ const storeSchema = new mongoose.Schema({
     photo: String
 });
 
-storeSchema.pre('save', function(next){ // pure function needed because we need this to be equal to be store that we are trying to save
+// make this async to see if stores with slug already exists
+storeSchema.pre('save', async function(next){ // pure function needed because we need this to be equal to be store that we are trying to save
     if(!this.isModified('name')){ // if stores name is not modified, just skip
         next();
         return; // stop the function from running
     }
     this.slug = slug(this.name); // to only run if name has been changed
+    // need to find other stores of the same base (store, store-1, store-2)
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i') // case insensitive
+    // starting with this.slug
+    // and ending in something optional (?) ( the optional thing will have a dash then some numerals repeating any number of times, and it then will be the end of the string)
+
+    // Store does not exist yet, but this.constructor will be Store once it runs
+    const storesWithSlug = await this.constructor.find({slug:slugRegEx});
+    if(storesWithSlug.length){
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
     next();
     // TODO make resiliant for unique slugs
 });
