bd15adeb0738f1b546fca476f3c4c79379845696
catbeans <workemailzoop@gmail.com>
2020-08-16 15:47:33 -0400

    Loading Sample Data

--- data/load-sample-data.js
+++ data/load-sample-data.js
@@ -1,33 +1,33 @@
-require('dotenv').config({ path: __dirname + '/../variables.env' });
+require('dotenv').config({ path: __dirname + '/../variables.env' }); // requires env variables since this is not really part of app
 const fs = require('fs');
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // connect to database
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 
 // import all of our models - they need to be imported only once
 const Store = require('../models/Store');
-const Review = require('../models/Review');
+// const Review = require('../models/Review'); // temporarily commenting out since Review is not set up
 const User = require('../models/User');
 
-
+// read the json files
 const stores = JSON.parse(fs.readFileSync(__dirname + '/stores.json', 'utf-8'));
-const reviews = JSON.parse(fs.readFileSync(__dirname + '/reviews.json', 'utf-8'));
+// const reviews = JSON.parse(fs.readFileSync(__dirname + '/reviews.json', 'utf-8'));
 const users = JSON.parse(fs.readFileSync(__dirname + '/users.json', 'utf-8'));
-
+// delete from db. npm run blowitallaway
 async function deleteData() {
   console.log('😢😢 Goodbye Data...');
   await Store.remove();
-  await Review.remove();
+  // await Review.remove();
   await User.remove();
   console.log('Data Deleted. To load sample data, run\n\n\t npm run sample\n\n');
   process.exit();
 }
-
+// insert to db. npm run sample
 async function loadData() {
   try {
     await Store.insertMany(stores);
-    await Review.insertMany(reviews);
+    // await Review.insertMany(reviews);
     await User.insertMany(users);
     console.log('👍👍👍👍👍👍👍👍 Done!');
     process.exit();
